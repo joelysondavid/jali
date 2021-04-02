a0d2ef1a14f04211156132f6e600ff51fb1546fc
joedav <joelyson.david@live.com>
2021-04-02 15:30:17 -0300

    Criando classes de modelo de dominio

---
+++ JaliBackend/JaliBackend.Dominio/Entidades/Base.cs
@@ -0,0 +1,7 @@
+﻿namespace JaliBackend.Dominio.Entidades
+{
+    public class Base
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ JaliBackend/JaliBackend.Dominio/Entidades/Livro.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace JaliBackend.Dominio.Entidades
+{
+    /// <summary>
+    /// Livro
+    /// </summary>
+    public class Livro : Base
+    {
+        /// <summary>
+        /// Título do livro
+        /// </summary>
+        public string Titulo { get; set; }
+
+        /// <summary>
+        /// Autor do livro
+        /// </summary>
+        public string Autor { get; set; }
+
+        /// <summary>
+        /// Genero do livro
+        /// </summary>
+        public Genero Genero { get; set; }
+
+        /// <summary>
+        /// Avaliação do livro de 0 a 10
+        /// </summary>
+        public int Avaliacao { get; set; }
+
+        /// <summary>
+        /// Breve resumo para lembraça de pontos relevantes do livro
+        /// </summary>
+        public string Resumo { get; set; }
+
+        /// <summary>
+        /// Data que iniciou a leitura do livro
+        /// </summary>
+        public DateTime DataInicioLeitura { get; set; }
+
+        /// <summary>
+        /// Data que terminou de ler o livro
+        /// </summary>
+        public DateTime DataConclusaoLeitura { get; set; }
+
+        /// <summary>
+        /// Lido/Lendo
+        /// </summary>
+        public LidoLendo Status { get; set; }
+    }
+
+    public enum LidoLendo
+    {
+        /// <summary>
+        /// Já Concluiu a leitura do livro
+        /// </summary>
+        Lido,
+
+        /// <summary>
+        /// Ainda está lendo o livro
+        /// </summary>
+        Lendo
+    }
+}
