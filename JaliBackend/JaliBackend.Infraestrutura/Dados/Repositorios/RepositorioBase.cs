7f1c9173c3475417287c16759c374c353806e6ce
joedav <joelyson.david@live.com>
2021-04-02 17:05:01 -0300

    Repositorios + git ignore funcionando

---
+++ JaliBackend/JaliBackend.Infraestrutura/Dados/Repositorios/RepositorioBase.cs
@@ -0,0 +1,118 @@
+﻿using JaliBackend.Dominio.Core.Interfaces.Repositorios;
+using JaliBackend.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JaliBackend.Infraestrutura.Dados.Repositorios
+{
+    /// <summary>
+    /// Repositorio Base
+    /// </summary>
+    public class RepositorioBase<T> : IRepositorioBase<T> where T : class
+    {
+        /// <summary>
+        /// Sqlcontext somente leitura
+        /// </summary>
+        private readonly SqlContext _sqlContext;
+
+        /// <summary>
+        /// Construtor recebe sqlcontext
+        /// </summary>
+        /// <param name="sqlContext"></param>
+        public RepositorioBase(SqlContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+
+        /// <summary>
+        /// Adicionar generico
+        /// </summary>
+        /// <param name="obj">Obj generico para adicionar</param>
+        /// <returns>Task</returns>
+        public async Task Adicionar(T obj)
+        {
+            try
+            {
+                await _sqlContext.Set<T>().AddAsync(obj);
+                await _sqlContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Atualizar generico
+        /// </summary>
+        /// <param name="obj">Obj generico para atualização</param>
+        /// <returns>Task</returns>
+        public async Task Atualizar(T obj)
+        {
+            try
+            {
+                _sqlContext.Entry(obj).State = EntityState.Modified;
+                await _sqlContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Remove obj generico
+        /// </summary>
+        /// <param name="obj">Objeto generico a ser removido</param>
+        /// <returns>Task</returns>
+        public async Task Remover(T obj)
+        {
+            try
+            {
+                _sqlContext.Set<T>().Remove(obj);
+                await _sqlContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Obter generico por id
+        /// </summary>
+        /// <param name="id">Chave identificadora</param>
+        /// <returns>Objeto encontrado</returns>
+        public async Task<T> ObterPorId(int id)
+        {
+            try
+            {
+                return await _sqlContext.Set<T>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Obtem todos os objetos do tipo passado
+        /// </summary>
+        /// <returns>Lista de objetos genericos</returns>
+        public async Task<IEnumerable<T>> ObterTodos()
+        {
+            try
+            {
+                return await _sqlContext.Set<T>().ToArrayAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ JaliBackend/JaliBackend.Infraestrutura/Dados/Repositorios/RepositorioLivro.cs
@@ -0,0 +1,25 @@
+﻿using JaliBackend.Dominio.Core.Interfaces.Repositorios;
+using JaliBackend.Dominio.Entidades;
+
+namespace JaliBackend.Infraestrutura.Dados.Repositorios
+{
+    /// <summary>
+    /// Repositorio Livro
+    /// </summary>
+    public class RepositorioLivro : RepositorioBase<Livro>, IRepositorioLivro
+    {
+        /// <summary>
+        /// Sql context
+        /// </summary>
+        private readonly SqlContext _sqlContext;
+
+        /// <summary>
+        /// Construtor com Sql context
+        /// </summary>
+        /// <param name="sqlContext">Sql context</param>
+        public RepositorioLivro(SqlContext sqlContext) : base(sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+    }
+}
